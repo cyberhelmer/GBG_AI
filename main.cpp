fead558071e78980582392cb2591dae9c1391634
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-11-25 16:29:56 +0100

    edit openai library to read api key from .env file using dot-env-cpp library

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 #include <iostream>
 
 int main() {
-  dotenv::init("api_key.env");
+
+ // dotenv::init("api_key.env");
   std::cout << "Hello, World!" << std::endl;
-  std::cout << "nu ska vi testa detta " << dotenv::getenv("JOHNSTEST", "hejhej") << std::endl;
+  openai::start();
+  auto chat = openai::chat().create(R"(
+    {
+        "model": "gpt-3.5-turbo",
+        "messages":[{"role":"user", "content":"Hej hur mår du?"}],
+        "max_tokens": 7,
+        "temperature": 0
+    }
+    )"_json);
+  std::cout << "Response is:\n" << chat.dump(2) << '\n';
   return 0;
 }
